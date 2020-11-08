082eb471fa3d855165b716431f8b1c581f62366e
Ania <zamora1607@users.noreply.github.com>
2020-11-08 09:00:29 +0100

    Add support to dark scheme - main view

--- fasthabit/fasthabit/views/MainView.swift
+++ fasthabit/fasthabit/views/MainView.swift
@@ -13,16 +13,20 @@ struct MainView: View {
     @EnvironmentObject var settings: UserSettings
     @State private var isPresented = false
     @State private var multiplier: CGFloat = 1.0
+    @State var nowDate: Date = Date()
+    
     @Environment(\.colorScheme) var colorScheme
     
-    @State var nowDate: Date = Date()
+    let gradientEnd = Color(red: 95.0/255, green: 169.0/255, blue: 244.0/255)
+    let gradientStart = Color(red: 79.0/255, green: 178.0/255, blue: 141.0/255)
     
     var body: some View {
         NavigationView {
             ZStack {
-                LinearGradient(gradient: Gradient(colors: [.blue, .white]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+                LinearGradient(gradient: Gradient(colors: [(colorScheme == .light ? .white : gradientStart), (colorScheme == .light ? gradientStart: gradientEnd)]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
                 
-                circleLayer
+                cirleLayer
+                    .animation(.linear(duration: 3))
                 
                 timerView
                     .navigationBarTitle(settings.userName.isEmpty ? "Hello Sunshine!" : "Hello \(settings.userName)")
@@ -60,11 +64,43 @@ struct MainView: View {
     
     var animationTimer: Timer {
         Timer.scheduledTimer(withTimeInterval: 2, repeats: true) {_ in
-            if self.multiplier == 0.9 {
+            if self.multiplier == 0.8 {
                 self.multiplier = 1.0
             } else {
-                self.multiplier = 0.9
+                self.multiplier = 0.8
             }
+        }
+    }
+    
+    var cirleLayer: some View {
+        ZStack {
+            bubble(color: gradientStart)
+                .frame(width:300 * multiplier)
+                .padding(50)
+            bubble(color: gradientStart)
+                .frame(width:50 * multiplier)
+                .offset(x: 150 * multiplier, y: 280)
+            bubble(color: gradientStart)
+                .frame(width:30 * multiplier)
+                .offset(x: -140 * multiplier, y: 130)
+            bubble(color: gradientStart)
+                .frame(width:40 * multiplier)
+                .offset(x: 100 * multiplier, y: -170)
+            bubble(color: gradientStart)
+                .frame(width:10 * multiplier)
+                .offset(x: -100, y: 190 * multiplier)
+        }
+    }
+    
+    var timerView: some View {
+        VStack {
+            Text("Time for \(isFastingTime ? "fasting" : "eating"). Left:")
+            Text(countDownString(from: nowDate, until: (isFastingTime ? self.settings.endTime : self.settings.startTime)))
+                .font(.largeTitle)
+                .onAppear(perform: {
+                    let _ = self.timer
+                    let _ = self.animationTimer
+                })
         }
     }
     
@@ -79,83 +115,24 @@ struct MainView: View {
         return String(format: "%02dh:%02dm:%02ds", abs(components.hour ?? 00), abs(components.minute ?? 00), abs(components.second ?? 00))
     }
     
-    var timerView: some View {
-        VStack {
-            Text("Time for \(isFastingTime ? "fasting" : "eating"). Left:")
-            Text(countDownString(from: nowDate, until: (isFastingTime ? self.settings.endTime : self.settings.startTime)))
-                .onAppear(perform: {
-                    let _ = self.timer
-                    let _ = self.animationTimer
-                }).modifier(Title())
-        }
-    }
-    
-    func bubble(color: Color, blendMode: BlendMode) -> some View  {
-        return Circle().fill(color).blendMode(blendMode)
-    }
-    
-    var circleLayer: some View {
-        ZStack {
-            firstCirleLayer
-                .animation(.linear(duration: 2))
-            secondCirleLayer
-                .animation(.spring())
-        }
-    }
-    
-    var firstCirleLayer: some View {
-        ZStack {
-            bubble(color: .green, blendMode: .softLight)
-                .frame(width:150)
-                .offset(x: -100 * multiplier, y: -100)
-            bubble(color: .green, blendMode: .screen)
-                .frame(width:300 * multiplier)
-                .padding(50)
-            bubble(color: .green, blendMode: .screen)
-                .frame(width:160)
-                .offset(x: 0 * multiplier, y: 150)
-            bubble(color: .green, blendMode: .screen)
-                .frame(width:50 * multiplier)
-                .offset(x: 150, y: 280)
-            bubble(color: .green, blendMode: .screen)
-                .frame(width:30 * multiplier)
-                .offset(x: -150, y: 160)
-            bubble(color: .green, blendMode: .softLight)
-                .frame(width:15)
-                .offset(x: -140, y: 190 * multiplier)
-        }
-    }
-    
-    var secondCirleLayer: some View {
-        ZStack {
-            bubble(color: .red, blendMode: .softLight)
-                .frame(width:100)
-                .offset(x: -130 * multiplier, y: 0)
-            bubble(color: .red, blendMode: .screen)
-                .frame(width:120)
-                .offset(x: 20 * multiplier, y: -170)
-            bubble(color: .red, blendMode: .softLight)
-                .frame(width:50 * multiplier)
-                .offset(x: 150, y: 100)
-            bubble(color: .red, blendMode: .screen)
-                .frame(width:30 * multiplier)
-                .offset(x: -130, y: 130)
-            bubble(color: .red, blendMode: .softLight)                .frame(width:15)
-                .offset(x: -140, y: 100 * multiplier)
-        }
+    func bubble(color: Color) -> some View  {
+        return Circle()
+            .fill(color)
+            .blendMode(colorScheme == .light ? .screen : .softLight)
     }
 }
 
-struct Title: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.black)
-    }
-}
-
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        MainView().environmentObject(UserSettings())
+        Group {
+            MainView()
+                .environmentObject(UserSettings())
+                .environment(\.colorScheme, .light)
+            MainView()
+                .environmentObject(UserSettings())
+                .environment(\.colorScheme, .dark)
+        }
     }
 }
+#endif
